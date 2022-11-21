6f7e5fbb9dc7d0e2f1d8c1f44baf42b9500fbacc
Eljen Mae Augusto <xt202001173@wmsu.edu.ph>
2022-11-21 17:43:56 +0800

    Commit_Changes
    Following the step of sir theo

--- index.php
+++ index.php
@@ -11,11 +11,15 @@
     <body>
         <h1>My Subjects</h1>
         <a href="forms/addform.php">Add Subject</a>
-        <table>
+        <a href="forms/addsyllabusform.php">Add Syllabus</a>
+        <table width='100%'>
             <tr>
                 <td>ID</td>
                 <td>Subject Code</td>
                 <td>Subject Name</td>
+                <td>Syllabus ID</td>
+                <td>Syllabus Code</td>
+                <td>Syllabus Author</td>
                 <td>Action</td>
             </tr>
             <?php 
@@ -24,9 +28,13 @@
                     echo "<td>".$res['Subject_ID']."</td>";
                     echo "<td>".$res['Subject_Code']."</td>";
                     echo "<td>".$res['Subject_Name']."</td>";
+                    echo "<td>".$res['syllabus_id']."</td>";
+                    echo "<td>".$res['syllabus_code']."</td>";
+                    echo "<td>".$res['syllabus_author']."</td>";
                     echo "<td><a href=\"forms/editform.php?id=$res[Subject_ID]\">Edit</a>  |
                             <a href=\"functions/delete.php?id=$res[Subject_ID]\"
                             onClick=\"return confirm('Are you sure you want to delete this?');\">Delete</a></td>";
+                    echo "</tr>";
                 }
             
             ?>
